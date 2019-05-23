65644fbd73e7a3930407a9e71610600b80901c30
nvezones <nvezones@gmail.com>
2019-05-23 16:19:45 +0530

    back press

--- app/src/main/java/com/example/sbpd_app/Home.java
+++ app/src/main/java/com/example/sbpd_app/Home.java
@@ -2,12 +2,35 @@ package com.example.sbpd_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    private long backpresstime;
+    Toast toastback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(backpresstime+2000>System.currentTimeMillis())
+        {
+            super.onBackPressed();
+            toastback.cancel();
+            return;
+
+        }
+        else
+        {
+            toastback=Toast.makeText(getBaseContext(),"Please back again to exit",Toast.LENGTH_SHORT);
+            toastback.show();
+        }
+        backpresstime=System.currentTimeMillis();
+
     }
 }
--- app/src/main/java/com/example/sbpd_app/MainActivity.java
+++ app/src/main/java/com/example/sbpd_app/MainActivity.java
@@ -7,11 +7,14 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     TextView textView;
+    private long backpresstime;
+    Toast toastback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,5 +42,22 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         t.start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(backpresstime+2000>System.currentTimeMillis())
+        {
+            super.onBackPressed();
+            toastback.cancel();
+            return;
+
+        }
+        else
+        {
+            toastback=Toast.makeText(getBaseContext(),"Please back again to exit",Toast.LENGTH_SHORT);
+            toastback.show();
+        }
+        backpresstime=System.currentTimeMillis();
     }
 }
