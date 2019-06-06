57545189dcea52a88ea33e6cd801a229623d49d2
nvezones <nvezones@gmail.com>
2019-06-06 16:57:52 +0530

    Merge branch 'master' of C:\Users\NIKHIL VERMA\SBPD_app with conflicts.

--- app/src/main/java/com/example/sbpd_app/Home.java
+++ app/src/main/java/com/example/sbpd_app/Home.java
@@ -12,12 +12,13 @@ import com.example.sbpd_app.Forms.Form1;
 import com.example.sbpd_app.Forms.PositionBattery;
 import com.example.sbpd_app.Forms.PositionOfIsolator;
 import com.example.sbpd_app.Forms.StatuOfKv;
+import com.example.sbpd_app.Forms.StatusOfMetering;
 
 public class Home extends AppCompatActivity {
 
     private long backpresstime;
     Toast toastback;
-    LinearLayout form1,isofrm,statuskvla;
+    LinearLayout form1,isofrm,statuskvla,statuskvmeter;
     CardView cardView,cardView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,7 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         form1=(LinearLayout)findViewById(R.id.form1);
         isofrm=(LinearLayout)findViewById(R.id.frmiso);
+        statuskvmeter=(LinearLayout)findViewById(R.id.statuskvmeter);
         statuskvla=(LinearLayout)findViewById(R.id.status11kv33kv);
         cardView=(CardView)findViewById(R.id.form2);
         form1.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +51,12 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), StatuOfKv.class));
+            }
+        });
+        statuskvmeter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), StatusOfMetering.class));
             }
         });
 
